da34523fc7b5257ce7b31531a0d34520313ca25a
b1994mi <b1994mi@gmail.com>
2021-02-01 22:11:20 +0700

    adds multer validation of files upload, max size and mimetype

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 var multer = require('multer');
-var upload = multer();
+var upload = multer({
+  limits: { fileSize: 1048576 }, // 1mb max
+  fileFilter: function (req, file, cb) {
+    const fileTypes = /png|jpeg|jpg|webp|gif/;
+    const mimeType = fileTypes.test(file.mimetype);
+    if (mimeType) {
+      cb(null, true);
+    } else {
+      cb("Error: only png, jpeg, and jpg are allowed!");
+    }
+  },
+});
 const PORT = process.env.PORT || 5000;
 const app = express();
 const reviewRoute = require("./routes/review");
